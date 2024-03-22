9e1ab1be1a0f010ad7e3dd14e1e3dfd88f522e3e
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-03-22 19:49:01 -0400

    test()[lexer+parser]: small test about lexer and parser

---
+++ __main__.py
@@ -0,0 +1,39 @@
+from cmp.pycompiler import Grammar
+from cmp.tools.lexer import Lexer
+
+# ===================================================== START TEST DEL LEXER ========================================================
+
+nonzero_digits = '|'.join(str(n) for n in range(1,10))
+letters = '|'.join(chr(n) for n in range(ord('a'),ord('z')+1))
+
+print('Non-zero digits:', nonzero_digits)
+print('Letters:', letters)
+
+G = Grammar()
+_num, _for, _foreach, _space, _id = G.Terminals('num, for, foreach, space, id')
+
+# En los tipos de tokens se deben usar los simbolos de la gramática
+lexer = Lexer([
+    ('num', f'({nonzero_digits})(0|{nonzero_digits})*'),
+    ('for' , 'for'),
+    ('foreach' , 'foreach'),
+    ('space', '  *'),
+    ('id', f'({letters})({letters}|0|{nonzero_digits})*')
+], 'eof')
+
+text = '5465 for 45foreach fore'
+print(f'\n>>> Tokenizando: "{text}"')
+
+tokens = lexer(text)
+print(tokens)
+assert [t.token_type for t in tokens] == ['num', 'space', 'for', 'space', 'num', 'foreach', 'space', 'id', 'eof']
+assert [t.lex for t in tokens] == ['5465', ' ', 'for', ' ', '45', 'foreach', ' ', 'fore', '$']
+
+text = '4forense forforeach for4foreach foreach 4for'
+print(f'\n>>> Tokenizando: "{text}"')
+tokens = lexer(text)
+print(tokens)
+assert [t.token_type for t in tokens] == ['num', 'id', 'space', 'id', 'space', 'id', 'space', 'foreach', 'space', 'num', 'for', 'eof']
+assert [t.lex for t in tokens] == ['4', 'forense', ' ', 'forforeach', ' ', 'for4foreach', ' ', 'foreach', ' ', '4', 'for', '$']
+
+# ===================================================== END TEST DEL LEXER ========================================================
