4e317c428309f57b43830cc8ab66c9fba85e8143
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-03-28 23:38:20 -0400

    feature()[ast]: add new ast nodes

--- cmp/tools/ast.py
+++ cmp/tools/ast.py
@@ -11,15 +11,9 @@ class StatementNode(Node):
 class ExpressionNode(Node):
     pass
 
-class AtomicNode(ExpressionNode):
-    def __init__(self, lex) -> None:
-        self.lex = lex
 
-class BinaryNode(ExpressionNode):
-    def __init__(self, lnode, rnode) -> None:
-        self.left = lnode
-        self.right = rnode
-        
+
+       
 # Declarations Nodes
 class VarsDeclarationsListNode(StatementNode):
     def __init__(self, declarations) -> None:
@@ -35,28 +29,34 @@ class VarAssignation(StatementNode):
         self.id = idx
         self.expr = expr
 
-
-class BlockNode(StatementNode):
-    def __init__(self, exprs) -> None:
-        self.exprs = exprs
-
-
 class FuncFullDeclarationNode(StatementNode):
     def __init__(self, idx, params, body) -> None:
         self.id = idx
         self.params = params
         self.body = body
     
- # Builtin Math functions
-
 class FuncInlineDeclarationNode(StatementNode):
     def __init__(self, idx, params, body) -> None:
         self.id = idx
         self.params = params
         self.body = body
     
- # Builtin Math functions
 
+
+
+
+class AtomicNode(ExpressionNode):
+    def __init__(self, lex) -> None:
+        self.lex = lex
+
+class BinaryNode(ExpressionNode):
+    def __init__(self, lnode, rnode) -> None:
+        self.left = lnode
+        self.right = rnode
+
+class BlockNode(ExpressionNode):
+    def __init__(self, exprs) -> None:
+        self.exprs = exprs
 
 class CallNode(ExpressionNode):
     def __init__(self, idx, args, obj=None, typex=None):
@@ -65,18 +65,28 @@ class CallNode(ExpressionNode):
         self.args = args
         self.type = typex
 
-  
-class ConstantNode(AtomicNode):
+
+
+
+
+class NumberNode(AtomicNode):
     pass
 
-class VariableNode(AtomicNode):
+class StringNode(AtomicNode):
     pass
 
 class BooleanNode(AtomicNode):
     pass
 
+class VariableNode(AtomicNode):
+    pass
+
+
+
+
 
 # Operations
+
 class PlusNode(BinaryNode):
     pass
 
@@ -92,41 +102,79 @@ class DivNode(BinaryNode):
 class PowNode(BinaryNode):
     pass
 
+class AndNode(BinaryNode):
+    pass
+
+class OrNode(BinaryNode):
+    pass
+
+class NotNode(AtomicNode):
+    pass
+
+class GreaterThatNode(BinaryNode):
+    pass
+
+class LessThatNode(BinaryNode):
+    pass
+
+class GreaterOrEqualThatNode(BinaryNode):
+    pass
+
+class LessOrEqualThatNode(BinaryNode):
+    pass
+
+class EqualNode(BinaryNode):
+    pass
+
+class NotEqualNode(BinaryNode):
+    pass
+
+
+class StringSimpleConcatNode(BinaryNode):
+    """<string> @ <string>"""
+    pass
+
+
+class StringSpaceConcatNode(BinaryNode):
+    """<string> @@ <string>"""
+    pass
+
+
+
+class WhileLoopNode(ExpressionNode):
+    def __init__(self, expr, body) -> None:
+        self.expr = expr
+        self.body = body
+
+class ForLoopNode(ExpressionNode):
+    def __init__(self, ) -> None:
+        super().__init__()
+
+class IfNode(ExpressionNode):
+    def __init__(self, if_expr, then_expr, elif_expr, else_expr) -> None:
+        self.if_expr = if_expr
+        self.then_expr = then_expr
+        self.elif_expr = elif_expr
+        self.else_expr = else_expr
 
 
 
 
 
 
+class TypeDeclarationNode(StatementNode):
+    def __init__(self, idx, args, attrs, funcs, inherit=None) -> None:
+        self.id = idx
+        self.args = args
+        self.attrs = attrs
+        self.funcs = funcs
+        self.inherit = inherit
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-   
-# class ClassDeclarationNode(StatementNode):
-#     def __init__(self, idx, features, parent=None) -> None:
-#         self.id = idx
-#         self.features = features
-#         self.parent = parent
-
-# class AttrDeclarationNode(StatementNode):
-#     def __init__(self, idx, typex, expr=None) -> None:
-#         self.id = idx
-#         self.type = typex
-#         self.expr = expr
+class AttrDeclarationNode(StatementNode):
+    def __init__(self, idx, typex, expr=None) -> None:
+        self.id = idx
+        self.type = typex
+        self.expr = expr
 
 # class ParamNode(StatementNode):
 #     def __init__(self, idx, typex) -> None:
@@ -134,45 +182,21 @@ class PowNode(BinaryNode):
 #         self.type = typex
 
 # Expressions Nodes
-class SimpleExpressionNode(ExpressionNode):
-    def __init__(self, expr) -> None:
-        self.expr = expr
-            
-class ConditionalNode(ExpressionNode):
-    def __init__(self, if_expr, then_expr, elif_expr, else_expr) -> None:
-        self.if_expr = if_expr
-        self.then_expr = then_expr
-        self.elif_expr = elif_expr
-        self.else_expr = else_expr
-
-class WhileNode(ExpressionNode):
-    def __init__(self, expr, body) -> None:
-        self.expr = expr
-        self.body = body
+# class SimpleExpressionNode(ExpressionNode):
+#     def __init__(self, expr) -> None:
+#         self.expr = expr
 
 # list of variables declarations
-class VarsDeclarationsListNode(ExpressionNode):
-    def __init__(self, decls, body) -> None:
-        self.decls = decls
-        self.body = body
+# class VarsDeclarationsListNode(ExpressionNode):
+#     def __init__(self, decls, body) -> None:
+#         self.decls = decls
+#         self.body = body
 
 # simple variable declaration
-class VarNode(ExpressionNode):
-    def __init__(self, idx, expr) -> None:
-        self.id = idx
-        self.expr = expr
 
-
-
-class ParenthesisExpr(ExpressionNode):
-    def __init__(self, expr) -> None:
-        self.expr = expr
-
-class BlockNode(ExpressionNode):
-    def __init__(self, exprs) -> None:
-        self.exprs = exprs
-
-        
+# class ParenthesisExpr(ExpressionNode):
+#     def __init__(self, expr) -> None:
+#         self.expr = expr
 
 
 
