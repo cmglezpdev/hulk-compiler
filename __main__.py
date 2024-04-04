39187cb7f8cd08a73424738b6199aaa83a9122db
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-04-03 20:40:59 -0400

    test()[]: execute specifics tests

--- __main__.py
+++ __main__.py
@@ -1,43 +1,87 @@
 import os
 import sys
-from cmp.core.lexer.scanner import build_lexer, semantic_checker, tokenizer
+import argparse
+from cmp.core.lexer.scanner import build_lexer, semantic_checker, tokenizer, type_checker
 from cmp.core.parser.parser import parse, build_parser
 from cmp.core.semantics  import SemanticCheckerVisitor
 from cmp.core.type_check import TypeCheckingVisitor
 
 
-
-def load_tests():
+def load_tests(left_test, right_test):
     files = os.listdir('./tests/')
     files = [file for file in files if file.endswith('.hulk')]
 
+    if left_test is None:
+        left_test = 1
+    
+    if right_test is None:
+        right_test = len(files)
+
+    print('>>> Execute tests between [%d, %d]'%(left_test, right_test))
+
     tests = []
     for file in files:
-        with open(f'./tests/{file}', 'r') as test:
-            tests.append((file, test.read()))
-
+        try:
+            test_id  = int(file.split('.')[1])
+            if left_test <= test_id and test_id <= right_test:
+                with open(f'./tests/{file}' , 'r') as test:
+                    tests.append((file, test.read()))
+        except:
+            print('Test with id %d not exists.' %(test_id))
+            continue
     return tests
 
+def load_args():
+    
+    arg_parser = argparse.ArgumentParser("Execute compiler")
+    arg_parser.add_argument('--test-id', type=int, help="Execute a specific test")
+    arg_parser.add_argument('--test-range', type=int, nargs=2, metavar=('start', 'end'), help="Execute test in the range especified")
+    
+    args = arg_parser.parse_args()
+    test_id = args.test_id
+    test_range = args.test_range
+    
+    if test_id is None and test_range is None:
+        return [None, None]
+    
+    if test_id is not None and test_range is not None:
+        raise Exception('You must provide just a test_id or a range, not both')
+    
+    if test_id is not None:
+        return [test_id, test_id]
+    
+    return test_range
+        
+
 if __name__ == '__main__':
-    tests = load_tests()
+    [left_test, right_test] = load_args()
+    tests = load_tests(left_test, right_test)
     lexer = build_lexer()
     parser = build_parser()
     sem_checker = SemanticCheckerVisitor()
-    type_checker = TypeCheckingVisitor()
+    t_checker = TypeCheckingVisitor()
 
     for file, content in tests:
-        print(f'Parsing code: {file}\n\n')
+        print(f'\n\n\nParsing code: {file}')
         
         code_tokens = tokenizer(content, lexer=lexer)
         ast = parse(code_tokens)
         serrors = semantic_checker(ast, sem_checker)
+        terrors = type_checker(ast)
         
-        if len(serrors) == 0:
-            terrors = type_checker.visit(ast)
-            print(terrors)
-
-        print(serrors)
-        sem_checker.errors =[]
+        if(len(serrors) > 0):
+            print(">>> %d errors founded:" %(len(serrors)))
+            for e in serrors:
+                print('* %d' %(e))
+        
+        if(len(terrors) > 0):
+            print(">>> %d errors founded:" %(len(terrors)))
+            for e in terrors:
+                print('* %d' %(e))
+        
+        
+        
+        sem_checker.errors = []
 
 
 
