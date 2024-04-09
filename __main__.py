8fb4285345f65364f1a15b71b442ac341805e4cb
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-04-08 21:00:53 -0400

    feature()[]: can execute specific hulk files

--- __main__.py
+++ __main__.py
@@ -33,39 +33,46 @@ def load_tests(left_test, right_test):
             continue
     return tests
 
+def load_file(file: str):
+    content = ''
+    with open(file, 'r') as f:
+        content = f.read()
+
+    return file, content
+
 def load_args():
-    
     arg_parser = argparse.ArgumentParser("Execute compiler")
+    arg_parser.add_argument('--file', type=str, help="Execute a hulk file")
     arg_parser.add_argument('--test-id', type=int, help="Execute a specific test")
     arg_parser.add_argument('--test-range', type=int, nargs=2, metavar=('start', 'end'), help="Execute test in the range especified")
     
     args = arg_parser.parse_args()
+    file = args.file    
     test_id = args.test_id
     test_range = args.test_range
-    
-    if test_id is None and test_range is None:
-        return [None, None]
-    
-    if test_id is not None and test_range is not None:
-        raise Exception('You must provide just a test_id or a range, not both')
-    
-    if test_id is not None:
-        return [test_id, test_id]
-    
-    return test_range
-        
+
+    return {
+        'file': file,
+        'test_id': test_id,
+        'test_range': test_range
+    }
+
 
 if __name__ == '__main__':
-    [left_test, right_test] = load_args()
-    tests = load_tests(left_test, right_test)
     lexer = build_lexer()
     parser = build_parser()
     sem_checker = SemanticCheckerVisitor()
     t_checker = TypeCheckingVisitor()
 
-    for file, content in tests[:6]:
+    args = load_args()
+    if args['file'] is not None or args['test_id'] is not None:
+        file, content = None, None
+        if args['file'] is not None:
+            file, content = load_file(args['file'])
+        else:
+            file, content = load_file(f'./tests/test.{args['test_id']}.hulk')        
+    
         print(f'\n\n\nParsing code: {file}')
-        
         code_tokens = tokenizer(content, lexer=lexer)
         ast = parse(code_tokens,parser=parser)
         
@@ -82,9 +89,7 @@ if __name__ == '__main__':
             print(">>> %d errors founded:" %(len(terrors)))
             for e in terrors:
                 print('* %s' %(e))
-        
-        
-        
+    
         sem_checker.errors = []
         
         print('--------------------------')
@@ -95,5 +100,45 @@ if __name__ == '__main__':
         cil_ast = hulkToCil.visit(ast, scope)
         formatter = get_formatter()
         print(formatter(cil_ast))
+        
+    else:
+        try:
+            left_test, right_test = args['test_range']
+        except:
+            left_test, right_test = None, None
+
+        tests = load_tests(left_test, right_test)
+        for file, content in tests[:6]:
+            print(f'\n\n\nParsing code: {file}')
+            
+            code_tokens = tokenizer(content, lexer=lexer)
+            ast = parse(code_tokens,parser=parser)
+            
+            context = type_collector(ast)
+            scope, serrors = semantic_checker(ast, sem_checker,context)
+            terrors = type_checker(ast, t_checker,context)
+            
+            if(len(serrors) > 0):
+                print(">>> %d errors founded:" %(len(serrors)))
+                for e in serrors:
+                    print('* %s' %(e))
+            
+            if(len(terrors) > 0):
+                print(">>> %d errors founded:" %(len(terrors)))
+                for e in terrors:
+                    print('* %s' %(e))
+            
+            
+            
+            sem_checker.errors = []
+            
+            print('--------------------------')
+            print(context)
+            print('--------------------------')
+
+            hulkToCil = HULKToCIL(context)
+            cil_ast = hulkToCil.visit(ast, scope)
+            formatter = get_formatter()
+            print(formatter(cil_ast))
 
 
